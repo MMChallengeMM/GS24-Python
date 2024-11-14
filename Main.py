04034a4df76361ce60b0c6b5060489c8d351fc71
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2024-11-14 20:35:18 -0300

    correção do menu, organização e criação de segundo ponto do menu

--- Main.py
+++ Main.py
@@ -1,16 +1,23 @@
+from datetime import datetime
+
+ultimos_calculos = []
+
 def calcular_emissao(consumo, fator):
     return consumo * fator
 
 
 def emissao_veiculos():
+    print("\nEssa primeira etapa é sobre seus meios de locomoção\n"
+          "Primeiro vou fazer algumas peguntas sobre seu veiculo pessoal.")
+
     while True:
         try:
-            print("\nCarro\n"
-                  "Qual o tipo de combustivel?\n"
+            print("\nQual o tipo de combustivel do seu veiculo?\n"
                   "1. Diesel\n"
                   "2. Gasolina\n"
                   "3. Elétrico\n"
-                  "4. Não possuo veiculo")
+                  "4. Não possuo veiculo\n"
+                  "===========================================================")
 
             veiculo_escolhido = int(input("Digite a opção desejada:\n"))
 
@@ -35,22 +42,24 @@ def emissao_veiculos():
     if veiculo_escolhido != 4:
         while True:
             try:
-                km_veiculo_pessoal = int(input("\nQuantos quilomêtros, em média, você dirige?\n"))
+                km_veiculo_pessoal = float(input("Quantos quilomêtros por semana, em média, você dirige?\n"))
                 break
             except ValueError:
-                print("Valor Inválido")
+                print("Valor inválido\n")
     else:
         km_veiculo_pessoal = 0
 
     emissao_veiculo_pessoal = calcular_emissao(consumo_veiculo_escolhido, km_veiculo_pessoal)
 
+    print("\nAgora vou fazer algumas peguntas sobre seu uso de transporte publico")
+
     while True:
         try:
-            print("\nTransporte Publico\n"
-                  "Qual o principal meio de transporte publico que você usa?\n"
+            print("Qual o principal meio de transporte publico que você usa?\n"
                   "1. Ônibus\n"
                   "2. Trem/Metro\n"
-                  "3. Não utilizo transporte publico")
+                  "3. Não utilizo transporte publico\n"
+                  "===========================================================")
 
             transporte_escolhido = int(input("Digite a opção desejada:\n"))
 
@@ -67,70 +76,76 @@ def emissao_veiculos():
                 case _:
                     print("Opção inválida")
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
 
     if transporte_escolhido != 3:
         while True:
             try:
-                km_transporte_publico = int(input("\nQuantos quilomêtros, em média, você anda de transporte público?\n"))
+                km_transporte_publico = float(input("Quantos quilomêtros por semana, em média, você anda de transporte público?\n"))
                 break
             except ValueError:
-                print("Valor Inválido")
+                print("Valor inválido\n")
     else:
         km_transporte_publico = 0
 
     emissao_transporte_publico = calcular_emissao(consumo_transporte_escolhido, km_transporte_publico)
 
-    while True:
-        try:
-            vezes_viagem_aviao = int(input("\nQuantas vezes você viaja de avião?\n"))
-            break
-        except ValueError:
-            print("Valor inválido")
+    print("\nPara finalizar essa etapa, vou fazer umas perguntas sobre viagens aéreas.")
 
     while True:
         try:
-            km_aviao = int(input("\nQual a distância média dos seus voos?"))
+            vezes_viagem_aviao = int(input("Quantas vezes por ano você viaja de avião?\n"))
             break
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
+
+    while True:
+        try:
+            km_aviao = float(input("Qual a distância média dos seus voos?\n"))
+            break
+        except ValueError:
+            print("Valor inválido\n")
     emissao_aviao = calcular_emissao(vezes_viagem_aviao * 0.09, km_aviao)
 
     return emissao_veiculo_pessoal + emissao_transporte_publico + emissao_aviao
 
 
 def emissao_energia():
+    print("\nEssa segunda etapa é sobre sua energia residencial\n"
+          "Primeiro vou fazer algumas perguntas sobre seu uso de energia.")
     while True:
         try:
-            kwh_mes = int(input("\nQuantos kWh você consome por mês?\n"))
+            kwh_mes = float(input("Quantos kWh você consome por mês?\n"))
             break
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
     emissao_energia_residencial = calcular_emissao(0.5, kwh_mes)
 
     while True:
         try:
-            m3_gas_natural_mes = int(input("\nQuantos m³ de gás natural você usa por mês?\n"))
+            m3_gas_natural_mes = float(input("Quantos m³ de gás natural você usa por mês?\n"))
             break
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
     emissao_gas_natural = calcular_emissao(2.2, m3_gas_natural_mes)
 
     while True:
         try:
-            kgs_glp_mes = int(input("\nQuantos kg de GLP você usa por mês?\n"))
+            kgs_glp_mes = float(input("Quantos kg de GLP você usa por mês?\n"))
             break
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
     emissao_glp_mes = calcular_emissao(3, kgs_glp_mes)
 
     return emissao_energia_residencial + emissao_gas_natural + emissao_glp_mes
 
 
 def emissao_alimentos():
+    print("\nEssa ultima etapa é sobre sua alimentação\n"
+          "Vou fazer algumas perguntas do seu consumo de alimento.")
     while True:
         try:
-            porcoes_carne_vermelha = int(input("\nQuantas vezes, por semana, você consome carne vermelha?\n"))
+            porcoes_carne_vermelha = int(input("Quantas vezes, por semana, você consome carne vermelha?\n"))
             break
         except ValueError:
             print("Valor inválido")
@@ -138,26 +153,26 @@ def emissao_alimentos():
 
     while True:
         try:
-            porcoes_frango = int(input("\nQuantas vezes, por semana, você consome frango?\n"))
+            porcoes_frango = int(input("Quantas vezes, por semana, você consome frango?\n"))
             break
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
     emissao_frango = calcular_emissao(1.1, porcoes_frango)
 
     while True:
         try:
-            porcoes_peixe = int(input("\nQuantas vezes, por semana, você consome peixe?\n"))
+            porcoes_peixe = int(input("Quantas vezes, por semana, você consome peixe?\n"))
             break
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
     emissao_peixe = calcular_emissao(1.5, porcoes_peixe)
 
     while True:
         try:
-            porcoes_ovos_laticinios = int(input("\nQuantas vezes, por semana, você consome ovos/laticinios?\n"))
+            porcoes_ovos_laticinios = int(input("Quantas vezes, por semana, você consome ovos/laticinios?\n"))
             break
         except ValueError:
-            print("Valor inválido")
+            print("Valor inválido\n")
     emissao_ovos_laticinios = calcular_emissao(0.5, porcoes_ovos_laticinios)
 
     return emissao_carne_vermelha + emissao_frango + emissao_peixe + emissao_ovos_laticinios
@@ -167,17 +182,27 @@ def iniciar_calculo():
     valor_emissao_veiculo = emissao_veiculos()
     valor_emissao_energia = emissao_energia()
     valor_emissao_alimento = emissao_alimentos()
+    total_emissao = valor_emissao_alimento + valor_emissao_energia + valor_emissao_veiculo
+
+    ultimos_calculos.append({
+        "data_criacao": datetime.now(), #.strftime("%d/%m/%Y"),
+        "valor_emissao_veiculo":valor_emissao_veiculo,
+        "valor_emissao_energia":valor_emissao_energia,
+        "valor_emissao_alimento":valor_emissao_alimento,
+        "total_emissao":total_emissao
+    })
     print(f"\nAqui estão seu niveis de emissão de carbono:\n"
           f"Você gerou {valor_emissao_veiculo} kg de CO2 em veiculos\n"
           f"Você gerou {valor_emissao_energia} kg de CO2 em energia\n"
           f"Você gerou {valor_emissao_alimento} kg de CO2 em alimentos\n"
-          f"Em geral você produziu {valor_emissao_alimento + valor_emissao_energia + valor_emissao_veiculo} kg de CO2")
+          f"Em geral você produziu {total_emissao} kg de CO2")
 
 
 while True:
     try:
-        print("\nBem vindo à calculadora de carbono\n"
-              "1. Iniciar Calculo\n"
+        print("\nBem vindo ao GreenPrint\n"
+              "1. Iniciar calculo de emissão de carbono\n"
+              "2. Ver dados"
               "0. Sair\n")
 
         opcao = int(input("Digite a opção desejada:\n"))
@@ -188,5 +213,9 @@ while True:
                 break
             case 1:
                 iniciar_calculo()
+            case 2:
+                print(ultimos_calculos)
+            case _:
+                print("Opção inválida")
     except ValueError:
         print("Valor inválido")
